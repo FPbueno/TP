0d3a4d90cd0c37355bd5ce4cd7689d52f2c8f76b
Fillipe Almeida <120722022+FPbueno@users.noreply.github.com>
2024-03-07 17:10:56 -0300

    Delete aula3/servidor directory

--- aula3/servidor/src/routes/matematica.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import controller from "../controllers/Matematica";
-
-const router = Router();
-
-router.get("/", controller.somar);
-router.post("/", controller.subtrair);
-
-export default router;
--- aula3/servidor/src/routes/texto.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import controller from "../controllers/Texto";
-
-const router = Router();
-
-router.get("/", controller.concatenar);
-
-export default router;
