89b431f04cf3757b61e43aa5a92f8d792f201774
Fillipe Almeida <120722022+FPbueno@users.noreply.github.com>
2024-03-08 17:48:00 -0300

    Add files via upload

---
+++ src2/exercicio2.ts
@@ -0,0 +1,18 @@
+class Retangulo {
+    base: number;
+    altura: number;
+    constructor(base: number, altura: number) {
+        this.base = base;
+        this.altura = altura;
+    }
+    area(): number {
+        return this.base * this.altura;
+    }
+    perimetro(): number {
+        return 2 * this.base + 2 * this.altura;
+    }
+}
+
+const m = new Retangulo(2,3);
+console.log('Area:',m.area());
+console.log('Perimetro:',m.perimetro());
